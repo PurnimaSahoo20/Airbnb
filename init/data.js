aef427ac5132555684c093c1c4362a7a766f5b7c
purnimasahoo235@gmail.com <purnimasahoo235@gmail.com>
2025-06-13 22:00:46 +0800

    Second commit

--- init/data.js
+++ init/data.js
@@ -5,7 +5,7 @@ const sampleListings = [
       "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://a0.muscache.com/im/pictures/miso/Hosting-45551916/original/41cee6f6-a47b-40d2-a41e-145f72ff897e.jpeg?im_w=720"
     },
     price: 1500,
     location: "Malibu",
